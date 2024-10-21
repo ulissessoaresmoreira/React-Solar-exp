22a050c9f0630af7a787c5d6be979950b96f2b26
ulissessoaresmoreira <ulissessm@gmail.com>
2024-10-21 21:23:42 +0100

    Estilização da página que lista os planetas

--- src/paginas/planetas.js
+++ src/paginas/planetas.js
@@ -12,18 +12,18 @@ export default function PaginaPlanetas() {
   return (
     <>
       <Container maxWidth="xl">
-        <Typography variant="h2" component="h1" textAlign={'center'}>
+        <Typography variant="h2" component="h1" textAlign={'center'}  p={2}>
           Planetas do sistema solar
         </Typography>
 
-        <Grid2 container spacing={2} mt={2}>
+        <Grid2 container spacing={2} pt={2}>
           {/* XS = 2 ITEMS*/}
           {/* SM = 3 ITEMS*/}
           {/* MD = 4 ITEMS*/}
           {/* LS = 6 ITEMS*/}
           {/* XL = 6 ITEMS*/}
           <Grid2 size={{xs: 6, sm: 4, md: 3, lg: 2, xl: 2}}>            
-            <Card>
+            <Card className="cardVidro">
               <Link to="/planeta/teste">
                 <CardActionArea>
                   <CardContent>
